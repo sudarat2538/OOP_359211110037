e45291344433d80e8bd6829118505387bbf8b2bf
sudarat2538 <sudaratthongkaew2538@gmail.com>
2017-11-19 09:49:12 +0700

    Testing upload to Github

---
+++ src/oop_lab/s359211110037/rmutsv/com/HelloWorld.java
@@ -0,0 +1,12 @@
+package oop_lab.s359211110037.rmutsv.com;
+
+public class HelloWorld
+{
+//    Create main method
+public static void main(String[] args) {
+    System.out.println("Hello world, I'm NUK");
+    System.out.println("My name is Sudarat Thongkaew");
+
+
+}//main
+}//class

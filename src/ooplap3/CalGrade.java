5c2a9d6ad78b7e36d280a09b58c218f1cf36595d
sudarat2538 <sudaratthongkaew2538@gmail.com>
2017-11-27 15:17:10 +0700

    Cal Grade

--- src/ooplap3/CalGrade.java
+++ src/ooplap3/CalGrade.java
@@ -1,9 +1,36 @@
 package ooplap3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CalGrade {
 //    score 0-49 grade F
 //    score 50-59 grade D
 //    score 60-69 grade C
 //    score 70-79 grade B
 //    score 80-100 grade A
-}
+public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print("Enter your Score :");
+    int score = Integer.parseInt(reader.readLine());
+    if (score <= 49){
+        System.out.println("You grade : F");
+    }
+    else if (score <= 59){
+        System.out.println("You grade :D");
+    }
+    else if (score <= 69){
+        System.out.println("You grade :C");
+    }
+    else if (score <= 79){
+        System.out.println("You grade :B");
+    }
+    else if (score >= 80){
+        System.out.println("You grade :A");
+    }
+
+
+}//main
+}//class
+

7ee129fc4ca01a9700c8fea22560c54971c4245c
sudarat2538 <sudaratthongkaew2538@gmail.com>
2017-11-27 16:36:18 +0700

    Loop Grade

--- src/ooplap3/CalGrade.java
+++ src/ooplap3/CalGrade.java
@@ -12,25 +12,35 @@ public class CalGrade {
 //    score 80-100 grade A
 public static void main(String[] args) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    System.out.print("Enter your Score :");
-    int score = Integer.parseInt(reader.readLine());
-    if (score < 50){
-        System.out.println("You grade : F");
-    }
-    else if (score <= 59){
-        System.out.println("You grade :D");
-    }
-    else if (score <= 69){
-        System.out.println("You grade :C");
-    }
-    else if (score <= 79){
-        System.out.println("You grade :B");
-    }
-    else  {
-        System.out.println("You grade :A");
-    }
+    int count =0;
+    int score =0;
+    for (int i=1;i<=3;i++){
+    System.out.print("Enter your Score (1-100):");
+    score = Integer.parseInt(reader.readLine());
+    if (score<0 ||score>100){
+       count++;
+       if (count==3){
+           System.out.println("System Error.");
+           break;
+       }
+    }else {
+        if (score < 50) {
+            System.out.println("You grade : F");
+        } else if (score <= 59) {
+            System.out.println("You grade :D");
+        } else if (score <= 69) {
+            System.out.println("You grade :C");
+        } else if (score <= 79) {
+            System.out.println("You grade :B");
+        } else {
+            System.out.println("You grade :A");
 
+        }
+        break;
 
+    }
+    }
 }//main
 }//class
+
 

6adf5c484c5887e261fa56152a774ee8e5355851
sudarat2538 <sudaratthongkaew2538@gmail.com>
2018-01-29 15:49:18 +0700

    CampusApp

---
+++ src/ooplap8/Saiyai.java
@@ -0,0 +1,14 @@
+package ooplap8;
+
+public class Saiyai implements Campus { //implements คือ
+    @Override
+    public void getCampusName() {
+        System.out.println("Saiyai Campus.");
+
+    }
+
+    @Override
+    public void getArea() {
+        System.out.println("Thugsong Nakhonsithammarat");
+    }
+}
